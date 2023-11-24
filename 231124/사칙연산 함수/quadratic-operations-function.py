93e322c81ca18c9d76200571293115c333e57ccb
Ahhyun, KANG <70644095+ahyun39@users.noreply.github.com>
2023-11-24 13:25:44 +0900

    Title: 사칙연산 함수, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231124/사칙연산 함수/quadratic-operations-function.py
@@ -0,0 +1,17 @@
+a, o, c = map(str,input().split())
+if o not in ['+', '-', '/', '*']:
+    print(False)
+else:
+    ans = 0
+    a = int(a)
+    c = int(c)
+
+    if o == '+':
+        ans = a + c
+    elif o == '-':
+        ans = a - c
+    elif o == '/':
+        ans = a / c
+    else:
+        ans = a * c
+    print(a, o, c, '=', ans)

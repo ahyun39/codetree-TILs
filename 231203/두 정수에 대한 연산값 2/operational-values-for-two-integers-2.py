2dba5857d058d36b3dee36e63dbb4b53051f5d97
Ahhyun, KANG <70644095+ahyun39@users.noreply.github.com>
2023-12-03 20:07:12 +0900

    Title: 두 정수에 대한 연산값 2, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231203/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.py
@@ -0,0 +1,12 @@
+def calc(a, b):
+    if a > b:
+        a *= 2
+        b += 10
+    else:
+        a += 10
+        b *= 2
+    return a, b
+
+a, b = map(int,input().split())
+a, b = calc(a, b)
+print(a, b)

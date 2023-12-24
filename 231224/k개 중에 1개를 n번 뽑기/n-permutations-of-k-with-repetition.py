84c07c055c5d0fdc8a9c911a8cfa19a5efefca7d
Ahhyun, KANG <70644095+ahyun39@users.noreply.github.com>
2023-12-24 18:35:03 +0900

    Title: k개 중에 1개를 n번 뽑기, Time: 243ms, Memory: 29MB, Status: Passed - CodeTree

--- 231224/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.py
+++ 231224/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.py
@@ -3,12 +3,12 @@ k, n = map(int,input().split())
 ans = []
 
 def back():
-    if len(ans) == n: # 배열의 길이를 확인
+    if len(ans) == n:
         print(" ".join(map(str, ans)))
         return 
-    for i in range(1, k+1): # 1 ~ N 까지
-        ans.append(i) # 배열 추가
-        back() # 재귀
+    for i in range(1, k+1):
+        ans.append(i)
+        back()
         ans.pop()
             
 back()

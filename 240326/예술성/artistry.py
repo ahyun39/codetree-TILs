e3fad2bf26572bffc57c6926a3fedd4d837d45c2
Ahhyun, KANG <70644095+ahyun39@users.noreply.github.com>
2024-03-26 14:54:59 +0900

    Title: 예술성, Time: 3316ms, Memory: 28MB, Status: Passed - CodeTree

--- 240326/예술성/artistry.py
+++ 240326/예술성/artistry.py
@@ -22,9 +22,9 @@ def make_group(board, group_board):
                     q.append((nx,ny))
         return cnt
 
-    now = board[0][0]
-    group = str(1)
-    now_group = group
+    now = board[0][0] # 현재 탐색 중인 숫자
+    group = str(1) # 그룹 
+    now_group = group # 그룹이 다른데 숫자가 같은 경우 같은 그룹으로 묶이는 것을 방지하기 위한 변수
     for i in range(n):
         for j in range(n):
             if group_board[i][j] == 0:
@@ -47,58 +47,56 @@ def score(board, groups, scores):
                             nx, ny = a + dx, b + dy
                             if is_valid(nx,ny) and group_board[nx][ny] == g2:
                                 lines += 1
-            if lines > 0:
+            if lines > 0: # 맞닿아 있는 변이 하나라도 있는 경우 점수를 구한다.
                 scores += (groups[g1][0] + groups[g2][0]) * groups[g1][1] * groups[g2][1] * lines 
     return scores 
 
 
-def rotate(board):
-    # 십자가 회전
+def rotate_cross(board): # 십자 모양 반시계 방향으로 90도 회전
     row = board[n//2]
     col = [board[i][n//2] for i in range(n)]
     board[n//2] = col
     for i in range(n):
-        board[i][n//2] = row[-i-1]
-    # 정사각형 회전
+        board[i][n//2] = row[-1-i]
+
+def rotate_square(board): # 십자 모양을 제외한 4개의 정사각형을 각각 시계 방향으로 90도 회전
+    def rotate_square_section(board,start_row, end_row, start_col, end_col,b):
+        b = deque(b)
+        while b:
+            for i in range(end_row-1,start_row-1, -1):
+                for j in range(start_col,end_col):
+                    board[j][i] = b.popleft()
+    # 좌측 상단
     b = []
     for i in range(n//2):
         b += board[i][:n//2]
-    b = deque(b)
-    while b:
-        for i in range(n//2-1,-1,-1):
-            for j in range(n//2):
-                board[j][i] = b.popleft()
+    rotate_square_section(board, 0, n//2, 0, n//2, b)
+    # 우측 상단 
     b = []
     for i in range(n//2):
         b += board[i][n//2+1:]
-    b = deque(b)
-    while b:
-        for i in range(n-1,n//2,-1):
-            for j in range(n//2):
-                board[j][i] = b.popleft()
+    rotate_square_section(board, n//2+1, n, 0, n//2, b)
+    # 좌측 하단
     b = []
-    for i in range(n//2+1,n):
+    for i in range(n//2+1, n):
         b += board[i][:n//2]
-    b = deque(b)
-    while b:
-        for i in range(n//2-1,-1,-1):
-            for j in range(n//2+1,n):
-                board[j][i] = b.popleft()
+    rotate_square_section(board, 0, n//2, n//2+1, n, b)
+    # 우측 하단
     b = []
-    for i in range(n//2+1,n):
+    for i in range(n//2+1, n):
         b += board[i][n//2+1:]
-    b = deque(b)
-    while b:
-        for i in range(n-1,n//2,-1):
-            for j in range(n//2+1,n):
-                board[j][i] = b.popleft()
+    rotate_square_section(board, n//2+1, n, n//2+1, n, b)
+
+def rotate(board):
+    rotate_cross(board)
+    rotate_square(board)
     return board
 
 for _ in range(4):
     group_board = [[0]*n for _ in range(n)]
     groups = {}
-    make_group(board,group_board)
-    scores = score(board,groups,0)
+    make_group(board,group_board) # 그룹으로 나눈 후
+    scores = score(board,groups,0) # 예술 점수를 구한다.
     answer += scores
-    board = rotate(board)
+    board = rotate(board) #회전
 print(answer)

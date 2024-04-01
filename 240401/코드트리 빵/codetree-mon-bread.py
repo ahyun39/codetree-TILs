868cbfceb3136a7ac8296369b44d86754810d4ef
Ahhyun, KANG <70644095+ahyun39@users.noreply.github.com>
2024-04-01 21:45:09 +0900

    Title: 코드트리 빵, Time: 5031ms, Memory: 26MB, Status: Time Limit Exceed - CodeTree

--- 240401/코드트리 빵/codetree-mon-bread.py
+++ 240401/코드트리 빵/codetree-mon-bread.py
@@ -20,12 +20,18 @@ def basecamp(idx):
     cx, cy = convenience_stores[idx][0]-1, convenience_stores[idx][1]-1
     px, py = 0, 0
     dist = 1e9
+    dist2 = 1e9
     for i in range(n):
         for j in range(n):
             if board[i][j] == 1 and check[i][j] != -1:
                 if dist > abs(cx - i) + abs(cy - j):
-                    dist = abs(cx - i) + abs(cy - j)
-                    px, py = i, j
+                    for dx, dy in [(-1,0),(0,-1),(0,1),(1,0)]:
+                        nx, ny = i + dx, j + dy
+                        if 0 <= nx < n and 0 <= ny < n and check[nx][ny] != -1:
+                            if dist2 > abs(cx - nx) + abs(cy - ny):
+                                dist = abs(cx - i) + abs(cy - j)
+                                dist2 = abs(cx - nx) + abs(cy - ny)
+                                px, py = i, j
     return px, py
 
 i = 1

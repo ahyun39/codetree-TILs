5657e2a67c9bb5d161d7941f73e6431a3b449850
Ahhyun, KANG <70644095+ahyun39@users.noreply.github.com>
2024-08-20 22:42:29 +0900

    Title: 행복한 수열의 개수, Time: 121ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240820/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240820/행복한 수열의 개수/number-of-happy-sequence.py
@@ -1,31 +1,23 @@
-n, m = map(int,input().split())
-graph = [list(map(int,input().split())) for _ in range(n)]
-ans = 0
-for i in range(n):
-    col_cnt = 1
-    now_col = -1
-    for j in range(n):
-        if col_cnt >= m:
-            break
-        if graph[i][j] != now_col:
-            col_cnt = 1
-        elif graph[i][j] == now_col:
-            col_cnt += 1
-        now_col = graph[i][j]
-    if col_cnt >= m:
-        ans += 1
+def count_consecutive_segments(graph, n, m):
+    count = 0
+    for i in range(n):
+        segment_length = 1
+        previous_value = graph[i][0]
+        for j in range(1, n):
+            if graph[i][j] == previous_value:
+                segment_length += 1
+            else:
+                segment_length = 1
+                previous_value = graph[i][j]
+            if segment_length >= m:
+                count += 1
+                break
+    return count
 
-for i in range(n):
-    row_cnt = 1
-    now_row = -1
-    for j in range(n):
-        if row_cnt >= m:
-            break
-        if graph[j][i] != now_row:
-            row_cnt = 1
-        elif graph[j][i] == now_row:
-            row_cnt += 1
-        now_row = graph[j][i]
-    if row_cnt >= m:
-        ans += 1
+n, m = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(n)]
+
+ans = count_consecutive_segments(graph, n, m)
+transposed_graph = list(zip(*graph))
+ans += count_consecutive_segments(transposed_graph, n, m)
 print(ans)

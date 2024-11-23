66e10c26e5e778ea85cb8c1cfc49aab2b75af9c2
Ahhyun, KANG <70644095+ahyun39@users.noreply.github.com>
2024-11-23 16:55:13 +0900

    Title: k번째로 신기한 문자열, Time: 49ms, Memory: 16MB, Status: Passed - CodeTree

--- 241123/k번째로 신기한 문자열/kth-special-string.py
+++ 241123/k번째로 신기한 문자열/kth-special-string.py
@@ -2,7 +2,8 @@ n, k, T = map(str, input().split())
 words = []
 for _ in range(int(n)):
     word = str(input())
-    if T in word:
-        words.append(word)
+    if len(word) >= len(T):
+        if word[:len(T)] == T:
+            words.append(word)
 words.sort()
 print(words[int(k)-1])

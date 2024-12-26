e57f97ff4d0b930228f841f3452c24808fe5d803
Ahhyun, KANG <70644095+ahyun39@users.noreply.github.com>
2024-12-26 13:11:51 +0900

    Title: DateTime to DateTime, Time: 41ms, Memory: 16MB, Status: Passed - CodeTree

--- 241226/DateTime to DateTime/datetime-to-datetime.py
+++ 241226/DateTime to DateTime/datetime-to-datetime.py
@@ -2,18 +2,23 @@ a, b, c = map(int, input().split())
 minutes = 0
 
 na, nb, nc = 11, 11, 11
-while True:
-    if (na, nb, nc) == (a, b, c):
-        break
-    
-    nc += 1
-    minutes += 1
 
-    if nc == 60:
-        nb += 1
-        nc = 0
-        if nb == 24:
-            na += 1
-            nb = 0
+if (na, nb, nc) >(a, b, c):
+    print(-1)
 
-print(minutes)
+else:
+    while True:
+        if (na, nb, nc) == (a, b, c):
+            break
+        
+        nc += 1
+        minutes += 1
+
+        if nc == 60:
+            nb += 1
+            nc = 0
+            if nb == 24:
+                na += 1
+                nb = 0
+
+    print(minutes)

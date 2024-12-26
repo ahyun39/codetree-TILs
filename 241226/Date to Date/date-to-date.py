b28c2aa4562ec0543e34fb5bc4f7a3588a9ef0ac
Ahhyun, KANG <70644095+ahyun39@users.noreply.github.com>
2024-12-26 13:05:38 +0900

    Title: Date to Date, Time: 76ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 241226/Date to Date/date-to-date.py
@@ -0,0 +1,18 @@
+#                  1.  2.  3.  4.  5.  6.  7.  8.  9. 10. 11. 12.
+num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+m1, d1, m2, d2 = map(int, input().split())
+days = 0
+
+while True:
+    d1 += 1
+    days += 1
+    if m1 == m2 and d1 == d2:
+        break
+
+    if d1 > num_of_days[m1]:
+        m1 += 1
+        d1 = 1
+        days += 1
+
+print(days)
